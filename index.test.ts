826311fee79267816f077f1c6c4772c796068487
Emi <emilanylegaspi@gmail.com>
2025-01-13 06:18:10 +1300

    Refactored code and tests to improve encapsulation

--- index.test.ts
+++ index.test.ts
@@ -1,88 +1,98 @@
-import { finishMatch, getMatchSummary, initialiseScoreboard, scoreboard, startNewMatch, updateMatchScore } from './index'
+import { Scoreboard } from './index'
 
-beforeEach(() => {
-    initialiseScoreboard()
-})
+describe('Scoreboard', () => {
+    let scoreboard: Scoreboard
 
-it('should start a new match and add it to the scoreboard', () => {
-    startNewMatch('Denmark', 'Germany')
-    expect(scoreboard.length).not.toBe(0)
+    beforeEach(() => {
+        scoreboard = new Scoreboard()
+    })
+
+    it('should start a new match and add it to the scoreboard', () => {
+        scoreboard.startNewMatch('Denmark', 'Germany')
+
+        const summary = scoreboard.getMatchSummary()
+        expect(summary.length).not.toBe(0)
+        
+        const currentMatch = summary[0]
+        expect(currentMatch.homeTeam).toBe('Denmark')
+        expect(currentMatch.awayTeam).toBe('Germany')
+        expect(currentMatch.homeTeamScore).toBe(0)
+        expect(currentMatch.awayTeamScore).toBe(0)
+    })
     
-    const currentMatch = scoreboard[0]
-    expect(currentMatch.homeTeam).toBe('Denmark')
-    expect(currentMatch.awayTeam).toBe('Germany')
-    expect(currentMatch.homeTeamScore).toBe(0)
-    expect(currentMatch.awayTeamScore).toBe(0)
-})
-
-it('should throw an error when starting a match with an invalid team name', () => {
-    expect(() => startNewMatch('Denmark', '')).toThrow('Invalid team name')
-})
-
-it('should update the score of a match', () => {
-    const matchId = startNewMatch('Denmark', 'Germany')
-    expect(matchId).not.toBeUndefined()
+    it('should throw an error when starting a match with an invalid team name', () => {
+        expect(() => scoreboard.startNewMatch('Denmark', '')).toThrow('Invalid team name')
+    })
     
-    updateMatchScore(matchId, 1, 0)
+    it('should update the score of a match', () => {
+        const matchId = scoreboard.startNewMatch('Denmark', 'Germany')
+        expect(matchId).not.toBeUndefined()
+        
+        scoreboard.updateMatchScore(matchId, 1, 0)
 
-    const currentMatch = scoreboard.find(match => match.matchId === matchId)
-    expect(currentMatch).not.toBeUndefined()
-    expect(currentMatch?.homeTeamScore).toBe(1)
-    expect(currentMatch?.awayTeamScore).toBe(0)
-})
-
-it('should throw an error when updating the score of an unknown match', () => {
-    const matchId = 'match_id'
-    expect(() => updateMatchScore(matchId, 1, 0)).toThrow(`Unable to update score. Match with ID ${matchId} not found`)
-})
-
-it('should throw an error when updating the score with an invalid score', () => {
-    const matchId = startNewMatch('Denmark', 'Germany')
-    expect(matchId).not.toBeUndefined()
+        const summary = scoreboard.getMatchSummary()
+        expect(summary.length).not.toBe(0)
     
-    expect(() => updateMatchScore(matchId, -1, 0)).toThrow('Invalid score')
-})
-
-it('should finish a match and remove it from the scoreboard', () => {
-    const matchId = startNewMatch('Denmark', 'Germany')
-    expect(matchId).not.toBeUndefined()
+        const currentMatch = summary.find(match => match.matchId === matchId)
+        expect(currentMatch).not.toBeUndefined()
+        expect(currentMatch?.homeTeamScore).toBe(1)
+        expect(currentMatch?.awayTeamScore).toBe(0)
+    })
     
-    finishMatch(matchId)
-
-    const finishedMatch = scoreboard.find(match => match.matchId === matchId)
-    expect(finishedMatch).toBeUndefined()
-})
-
-it('should finish an unknown match', () => {
-    const matchId = 'match_id'
-    finishMatch(matchId)
-
-    const finishedMatch = scoreboard.find(match => match.matchId === matchId)
-    expect(finishedMatch).toBeUndefined()
-})
-
-it('should get the match summary with different total scores', () => {
-    const matchId1 = startNewMatch('Denmark', 'Germany')
-    updateMatchScore(matchId1, 1, 0)
+    it('should throw an error when updating the score of an unknown match', () => {
+        const matchId = 'match_id'
+        expect(() => scoreboard.updateMatchScore(matchId, 1, 0)).toThrow(`Unable to update score. Match with ID ${matchId} not found`)
+    })
     
-    const matchId2 = startNewMatch('Netherlands', 'Japan')
-    updateMatchScore(matchId2, 1, 2)
-
-    const summary = getMatchSummary()
-    expect(summary.length).toBe(2)
-    expect(summary[0].homeTeam).toBe('Netherlands')
-    expect(summary[1].homeTeam).toBe('Denmark')
-})
-
-it('should get the match summary with the same total scores', () => {
-    const matchId1 = startNewMatch('Denmark', 'Germany')
-    updateMatchScore(matchId1, 1, 0)
+    it('should throw an error when updating the score with an invalid score', () => {
+        const matchId = scoreboard.startNewMatch('Denmark', 'Germany')
+        expect(matchId).not.toBeUndefined()
+        
+        expect(() => scoreboard.updateMatchScore(matchId, -1, 0)).toThrow('Invalid score')
+    })
     
-    const matchId2 = startNewMatch('Netherlands', 'Japan')
-    updateMatchScore(matchId2, 0, 1)
-
-    const summary = getMatchSummary()
-    expect(summary.length).toBe(2)
-    expect(summary[0].homeTeam).toBe('Denmark')
-    expect(summary[1].homeTeam).toBe('Netherlands')
+    it('should finish a match and remove it from the scoreboard', () => {
+        const matchId = scoreboard.startNewMatch('Denmark', 'Germany')
+        expect(matchId).not.toBeUndefined()
+        
+        scoreboard.finishMatch(matchId)
+    
+        const summary = scoreboard.getMatchSummary()
+        expect(summary.length).toBe(0)
+    })
+    
+    it('should finish an unknown match', () => {
+        const matchId = 'match_id'
+        scoreboard.finishMatch(matchId)
+    
+        const summary = scoreboard.getMatchSummary()
+        expect(summary.length).toBe(0)
+    })
+    
+    it('should get the match summary with different total scores', () => {
+        const matchId1 = scoreboard.startNewMatch('Denmark', 'Germany')
+        scoreboard.updateMatchScore(matchId1, 1, 0)
+        
+        const matchId2 = scoreboard.startNewMatch('Netherlands', 'Japan')
+        scoreboard.updateMatchScore(matchId2, 1, 2)
+    
+        const summary = scoreboard.getMatchSummary()
+        expect(summary.length).toBe(2)
+        expect(summary[0].homeTeam).toBe('Netherlands')
+        expect(summary[1].homeTeam).toBe('Denmark')
+    })
+    
+    it('should get the match summary with the same total scores', () => {
+        const matchId1 = scoreboard.startNewMatch('Denmark', 'Germany')
+        scoreboard.updateMatchScore(matchId1, 1, 0)
+        
+        const matchId2 = scoreboard.startNewMatch('Netherlands', 'Japan')
+        scoreboard.updateMatchScore(matchId2, 0, 1)
+    
+        const summary = scoreboard.getMatchSummary()
+        expect(summary.length).toBe(2)
+        expect(summary[0].homeTeam).toBe('Denmark')
+        expect(summary[1].homeTeam).toBe('Netherlands')
+    })
+    
 })
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-type Match = {
+interface Match {
 	matchId: string
 	homeTeam: string
 	awayTeam: string
@@ -7,77 +7,76 @@ type Match = {
 	startTime: number
 }
 
-export let scoreboard: Match[] = []
-export const initialiseScoreboard = () => {
-	scoreboard = []
-}
+export class Scoreboard {
+	private scoreboard: Match[] = []
 
-/**
- * Starts a new match and adds it to the scoreboard.
- * This assumes that the initial scores for the teams are set at 0-0. 
- * @param homeTeam the home team name
- * @param awayTeam the away team name
- * @returns the match ID
- */
-export const startNewMatch = (homeTeam: string, awayTeam: string): string => {
-	if (homeTeam === '' || awayTeam === '') throw new Error('Invalid team name')
+	/**
+	 * Starts a new match and adds it to the scoreboard.
+	 * This assumes that the initial scores for the teams are set at 0-0. 
+	 * @param homeTeam the home team name
+	 * @param awayTeam the away team name
+	 * @returns the match ID
+	 */
+	startNewMatch(homeTeam: string, awayTeam: string): string {
+		if (homeTeam === '' || awayTeam === '') throw new Error('Invalid team name')
 
-	const newMatch: Match = {
-		matchId: crypto.randomUUID(),
-		homeTeam,
-		awayTeam,
-		homeTeamScore: 0,
-		awayTeamScore: 0,
-		startTime: new Date().getTime(),
+		const newMatch: Match = {
+			matchId: crypto.randomUUID(),
+			homeTeam,
+			awayTeam,
+			homeTeamScore: 0,
+			awayTeamScore: 0,
+			startTime: new Date().getTime(),
+		}
+		this.scoreboard.push(newMatch)
+
+		return newMatch.matchId
 	}
-	scoreboard.push(newMatch)
 
-	return newMatch.matchId
+	/**
+	 * Updates the scores for an ongoing match.
+	 * @param matchId the match ID
+	 * @param homeTeamScore the home team score
+	 * @param awayTeamScore the away team score
+	 */
+	updateMatchScore(matchId: string, homeTeamScore: number, awayTeamScore: number) {
+		const currentMatch = this.scoreboard.find(match => match.matchId === matchId)
+		if (!currentMatch) throw new Error(`Unable to update score. Match with ID ${matchId} not found`)
+		
+		if (homeTeamScore < 0 || awayTeamScore < 0) throw new Error('Invalid score')
+		
+		const updatedMatch: Match = { ...currentMatch, homeTeamScore, awayTeamScore }
+		const updatedScoreboard = this.scoreboard.filter(match => match.matchId !== matchId)
+
+		this.scoreboard = [...updatedScoreboard, updatedMatch]
+	}
+
+	/**
+	 * Finishes a match in progress and removes it from the scoreboard.
+	 * @param matchId the match ID
+	 */
+	finishMatch(matchId: string) {
+		this.scoreboard = this.scoreboard.filter(match => match.matchId !== matchId)
+	}
+
+	/**
+	 * Gets the match summary and orders the matches based on:
+	 * - total score (descending)
+	 * - start time (ascending)
+	 * @returns the match summary
+	 */
+	getMatchSummary(): Match[] {
+		return this.scoreboard
+			.map(match => ({
+				match,
+				totalScore: match.homeTeamScore + match.awayTeamScore
+			}))
+			.sort((a, b) => {
+				if (a.totalScore === b.totalScore) {
+					return a.match.startTime - b.match.startTime
+				}
+				return b.totalScore - a.totalScore
+			})
+			.map(match => match.match)
+	}
 }
-
-/**
- * Updates the scores for an ongoing match.
- * @param matchId the match ID
- * @param homeTeamScore the home team score
- * @param awayTeamScore the away team score
- */
-export const updateMatchScore = (matchId: string, homeTeamScore: number, awayTeamScore: number) => {
-	const currentMatch = scoreboard.find(match => match.matchId === matchId)
-	if (!currentMatch) throw new Error(`Unable to update score. Match with ID ${matchId} not found`)
-	
-	if (homeTeamScore < 0 || awayTeamScore < 0) throw new Error('Invalid score')
-	
-	const updatedMatch: Match = { ...currentMatch, homeTeamScore, awayTeamScore }
-	const updatedScoreboard = scoreboard.filter(match => match.matchId !== matchId)
-
-	scoreboard = [...updatedScoreboard, updatedMatch]
-}
-
-/**
- * Finishes a match in progress and removes it from the scoreboard.
- * @param matchId the match ID
- */
-export const finishMatch = (matchId: string) => {
-	scoreboard = scoreboard.filter(match => match.matchId !== matchId)
-}
-
-/**
- * Gets the match summary and orders the matches based on:
- * - total score (descending)
- * - start time (ascending)
- * @returns the match summary
- */
-export const getMatchSummary = (): Match[] => (
-	scoreboard
-		.map(match => ({
-			match,
-			totalScore: match.homeTeamScore + match.awayTeamScore
-		}))
-		.sort((a, b) => {
-			if (a.totalScore === b.totalScore) {
-				return a.match.startTime - b.match.startTime
-			}
-			return b.totalScore - a.totalScore
-		})
-		.map(match => match.match)
-)
